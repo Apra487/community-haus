34561f93344c77c478b98f4b7aa6eae6649ccf3c
percxno <161191884+percxno@users.noreply.github.com>
2024-04-06 13:23:07 +0530

    feat: initialize nextjs project

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      primary: {
+        DEFAULT: '#fff',
+        dark: '#000',
+      },
+      secondary: 'rgba(255, 255, 255, 0.60)',
+      accent: '#8EFF01',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0.75rem',
+        md: '1.5rem',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
