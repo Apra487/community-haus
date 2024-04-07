c6a29b4efa2f4db491357be99ef337db97eae9b1
Apra487 <apra487@gmail.com>
2024-04-08 02:05:35 +0530

    feat:add dynamic route and get data by username

---
+++ src/app/[username]/page.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from 'next';
+
+async function getCommunityByUserame(username: string) {
+  const res = await fetch(`http:localhost:3000/api/community?username=${username}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.json();
+}
+interface Props {
+  params: {
+    username: string;
+  };
+}
+
+const Username: NextPage<Props> = async ({ params }) => {
+  const username = params.username;
+  const data = await getCommunityByUserame(username);
+  console.log(data);
+
+  return (
+    <main className="container flex flex-col justify-center mt-10">
+      <div className="fixed w-screen h-screen bg-creator top-0 right-0 bg-no-repeat bg-cover -z-50"></div>
+      <h1>hola</h1>
+    </main>
+  );
+};
+
+export default Username;
