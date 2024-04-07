bf7b5d018577e267496fc21a456181a1a641b163
Apra487 <apra487@gmail.com>
2024-04-08 00:19:58 +0530

    feat: get by username

--- src/app/api/community/route.ts
+++ src/app/api/community/route.ts
@@ -3,17 +3,45 @@ import { mongoClient } from '@/utils/mongodb';
 export async function GET(request: Request) {
   // get request params
   const { searchParams } = new URL(request.url);
-  const contractAdress = searchParams.get('contractAdress');
+  const contractAddress = searchParams.get('contractAddress');
+  const username = searchParams.get('username');
 
-  await mongoClient.connect();
-  const database = mongoClient.db('community_haus');
-  const collection = database.collection('creater_groups');
+  // if nothing is provided, return say provide username or contract address
+  if (!username && !contractAddress) {
+    return Response.json({
+      message:
+        'Please provide a username or contractAddress as query parameter.',
+    });
+  }
 
-  const document = await collection.findOne({
-    contractAddress: contractAdress,
-  });
+  if (username) {
+    await mongoClient.connect();
+    const database = mongoClient.db('community_haus');
+    const collection = database.collection('creater_groups');
+
+    const document = await collection.findOne({
+      username: username,
+    });
+
+    return Response.json({
+      document,
+    });
+  }
+  if (contractAddress) {
+    await mongoClient.connect();
+    const database = mongoClient.db('community_haus');
+    const collection = database.collection('creater_groups');
+
+    const document = await collection.findOne({
+      contractAddress: contractAddress,
+    });
+
+    return Response.json({
+      document,
+    });
+  }
 
   return Response.json({
-    document,
+    message: 'GET ENDPOINT WORKS!',
   });
 }
