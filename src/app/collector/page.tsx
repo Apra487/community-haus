85974efd69f9a506f4a9581787b34d85afd63c40
percxno <161191884+percxno@users.noreply.github.com>
2024-04-08 00:55:14 +0530

    feat: get mint address array for user

--- src/app/collector/page.tsx
+++ src/app/collector/page.tsx
@@ -1,94 +1,23 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useWallet } from '@solana/wallet-adapter-react';
+
 import { ConnectButton } from '@/components/buttons';
-import { Metaplex } from '@metaplex-foundation/js';
-import { useEffect } from 'react';
-import { PublicKey, Connection } from '@solana/web3.js';
 
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import {
-  mplTokenMetadata,
-  fetchDigitalAsset,
-  fetchMetadata,
-  fetchJsonMetadata,
-} from '@metaplex-foundation/mpl-token-metadata';
-import { publicKey } from '@metaplex-foundation/umi-public-keys';
+export default function Collector() {
+  const { publicKey } = useWallet();
+  const [mintAddressArray, setMintAddressArray] = useState([]);
 
-const getNftData = async () => {
-  const connection = new Connection(
-    `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`
-  );
-  // const umi = createUmi(
-  //   `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`
-  // ).use(mplTokenMetadata());
-  const metaplex = new Metaplex(connection);
-
-  const ownerAddress = new PublicKey(
-    '8GqDCawh25igLzRF8o77sevix3VLnqBvWxhcJWh6E2bX'
-  );
-  metaplex
-    .nfts()
-    .findAllByOwner({ owner: ownerAddress })
-    .then((nfts) => {
-      console.log(nfts);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  // const umiPublicKey = publicKey(
-  //   '8GqDCawh25igLzRF8o77sevix3VLnqBvWxhcJWh6E2bX'
-  // );
-  // fetchDigitalAsset(umi, umiPublicKey).then((digitalAsset) => {
-  //   console.log(digitalAsset);
-  // });
-  // const metadataPublicKey = publicKey(
-  //   '3ouMw4NcDDSHq4tmApJnC5YL8aDR3mSqvvok6ekwZEvp'
-  // );
-  // fetchMetadata(umi, metadataPublicKey).then((metadata) => {
-  //   console.log(metadata);
-  // });
-  // fetchJsonMetadata(
-  //   umi,
-  //   'https://arweave.net/YZv_34JAzpmLiV9CEy2-MlF_xFYiNXSHsUp1DedQxCA'
-  // ).then((jsonMetadata) => {
-  //   console.log(jsonMetadata);
-  // });
-};
-
-export default async function Collector() {
-  await getNftData();
-  // useEffect(() => {
-  // const mintAddress = new PublicKey(
-  //   '8GqDCawh25igLzRF8o77sevix3VLnqBvWxhcJWh6E2bX'
-  // );
-  // metaplex
-  //   .nfts()
-  //   .findByMint({ mintAddress: mintAddress })
-  //   .then((nfts) => {
-  //     console.log(nfts);
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
-  // const umiPublicKey = publicKey(
-  //   '8GqDCawh25igLzRF8o77sevix3VLnqBvWxhcJWh6E2bX'
-  // );
-  // fetchDigitalAsset(umi, umiPublicKey).then((digitalAsset) => {
-  //   console.log(digitalAsset);
-  // });
-  // const metadataPublicKey = publicKey(
-  //   '3ouMw4NcDDSHq4tmApJnC5YL8aDR3mSqvvok6ekwZEvp'
-  // );
-  // fetchMetadata(umi, metadataPublicKey).then((metadata) => {
-  //   console.log(metadata);
-  // });
-  // fetchJsonMetadata(
-  //   umi,
-  //   'https://arweave.net/YZv_34JAzpmLiV9CEy2-MlF_xFYiNXSHsUp1DedQxCA'
-  // ).then((jsonMetadata) => {
-  //   console.log(jsonMetadata);
-  // });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
+  useEffect(() => {
+    if (publicKey) {
+      const address = publicKey.toBase58();
+      fetch(`/api/wallet-nft?address=${address}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setMintAddressArray(data.mintAddressArray);
+        });
+    }
+  }, [publicKey]);
   return (
     <main className="container flex flex-col justify-center mt-10">
       <div className="fixed w-screen h-screen bg-collector top-0 right-0 bg-no-repeat bg-cover -z-50"></div>
