4bace53494c5122ba70abf7b55db2233666007eb
percxno <161191884+percxno@users.noreply.github.com>
2024-04-08 10:22:04 +0530

    feat: update collector page

--- src/app/collector/page.tsx
+++ src/app/collector/page.tsx
@@ -22,12 +22,33 @@ export default function Collector() {
   }, [publicKey]);
   return (
     <main className="container flex flex-col justify-center mt-10">
-      <div className="fixed w-screen h-screen bg-collector top-0 right-0 bg-no-repeat bg-cover -z-50"></div>
       {!publicKey && (
-        <>
-          <h1>check your eligibility to join x person community</h1>
+        <div className="flex flex-col items-baseline justify-center fixed w-screen h-screen bg-collector top-0 right-0 bg-no-repeat bg-cover -z-50">
+          <div className="ml-32">
+            <span
+              style={{ lineHeight: '75px' }}
+              className="bg-transparent font-bold text-5xl max-md:text-4xl max-md:leading-[64px]"
+            >
+              Check Your{' '}
+            </span>
+            <section className="bg-accent inline text-5xl font-normal text-[rgb(0,0,0)]  max-md:text-4xl max-md:leading-[64px] ">
+              <span className="bg-accent inline font-bold text-zinc-900">
+                Eligibility{' '}
+              </span>
+            </section>
+            <br></br>
+            <span
+              style={{ lineHeight: '75px' }}
+              className="bg-transparent font-bold text-5xl max-md:text-4xl max-md:leading-[64px]"
+            >
+              To Join X Person<br></br> Community
+            </span>
+          </div>
           <ConnectButton />
-        </>
+        </div>
+      )}
+      {publicKey && (
+        <div className="fixed w-screen h-screen bg-collector top-0 right-0 bg-no-repeat bg-cover -z-50"></div>
       )}
       {communityArray.length === 0 && publicKey && <>Loading...</>}
       {communityArray.length > 0 && publicKey && (
