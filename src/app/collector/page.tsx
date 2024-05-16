30de9266cabf840aee2d0ed2d3aac3b34e4bd927
percxno <161191884+percxno@users.noreply.github.com>
2024-05-16 11:25:39 +0530

    feat: show communities collector can join

--- src/app/collector/page.tsx
+++ src/app/collector/page.tsx
@@ -1,24 +1,43 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, FC } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useWallet } from '@solana/wallet-adapter-react';
 
 import { ConnectButton } from '@/components/buttons';
 
+const JoinButton: FC<{
+  isJoining: boolean;
+  onClick: () => void;
+}> = ({ isJoining, onClick }) => {
+  return (
+    <button className="btn-primary h-12" onClick={onClick}>
+      {isJoining ? 'Joining...' : 'Join'}
+    </button>
+  );
+};
+
 export default function Collector() {
   const router = useRouter();
   const { publicKey } = useWallet();
   const [communityArray, setCommunityArray] = useState([]);
+  const [isFetchingEligibleGroups, setIsFetchingEligibleGroups] =
+    useState(false);
+  const [isJoining, setIsJoining] = useState(false);
+
   useEffect(() => {
+    async function fetchEligibleGroups(address: string) {
+      setIsFetchingEligibleGroups(true);
+      const response = await fetch(`/api/eligible-group?address=${address}`);
+      const data = await response.json();
+      console.log(data.eligibleGroups);
+      setCommunityArray(data.eligibleGroups);
+      setIsFetchingEligibleGroups(false);
+    }
+
     if (publicKey) {
       const address = publicKey.toBase58();
-      fetch(`/api/eligible-group?address=${address}`)
-        .then((res) => res.json())
-        .then((data: any) => {
-          console.log(data.eligibleGroups);
-          setCommunityArray(data.eligibleGroups);
-        });
+      fetchEligibleGroups(address);
     }
   }, [publicKey]);
 
@@ -67,8 +86,17 @@ export default function Collector() {
       {publicKey && (
         <div className="fixed w-screen h-screen bg-collector top-0 right-0 bg-no-repeat bg-cover -z-50">
           {
-            <div className="ml-32 mt-48 flex flex-col gap-5 overflow-y-scroll">
-              {communityArray.length === 0 && publicKey && <>Loading...</>}
+            <div className="container mt-48 flex flex-col gap-5 h-3/4 overflow-y-scroll">
+              {communityArray.length === 0 &&
+                publicKey &&
+                isFetchingEligibleGroups && (
+                  <h1 className="font-semibold text-xl">Loading...</h1>
+                )}
+              {communityArray.length === 0 &&
+                publicKey &&
+                !isFetchingEligibleGroups && (
+                  <h1 className="font-semibold text-xl">{`You're not eligible to join any community. Try collecting some NFTs :)`}</h1>
+                )}
               {communityArray.length > 0 && publicKey && (
                 <>
                   <h1 className="font-semibold text-xl">
@@ -78,10 +106,29 @@ export default function Collector() {
                   {communityArray.map((community: any) => (
                     <div
                       key={community._id}
-                      className="bg-tertiary p-4 rounded-2xl"
+                      className="bg-tertiary p-4 rounded-2xl flex flex-row justify-between items-center gap-5"
                     >
-                      <h2>{community.username}</h2>
-                      <p>{community.communityDescription}</p>
+                      <div>
+                        <div className="flex flex-row gap-1 items-baseline">
+                          <h5 className="font-semibold">
+                            {community.communityName}
+                          </h5>
+                          <h5 className="text-xs text-secondary">{`@${community.username}`}</h5>
+                        </div>
+                        <p className="text-secondary">
+                          {community.communityDescription}
+                        </p>
+                      </div>
+                      <JoinButton
+                        isJoining={isJoining}
+                        onClick={() => {
+                          setIsJoining(true);
+                          const superUsername =
+                            community.username.split('-')[0];
+                          router.push(`/communities/${superUsername}`);
+                          setIsJoining(false);
+                        }}
+                      />
                     </div>
                   ))}
                 </>
